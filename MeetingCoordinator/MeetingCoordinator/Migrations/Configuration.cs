e9170b07db9f96e23c04c599bb5f71c9575ce379
wcc17 <christian.curry17@gmail.com>
2016-04-26 22:33:53 -0400

    seed data in Configuration.cs should be working and updating the database properly now

--- MeetingCoordinator/MeetingCoordinator/Migrations/Configuration.cs
+++ MeetingCoordinator/MeetingCoordinator/Migrations/Configuration.cs
@@ -36,40 +36,47 @@ namespace MeetingCoordinator.Migrations
             //wes - password1234
             //melinda64 - hotdog
             //talnius - hotdog1234
-        //    context.Attendees.AddOrUpdate(
-        //        a => a.Username,
-        //        new Models.Attendee { FirstName = "William", LastName = "Curry", Username = "wcc17", Password = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8" },
-        //        new Models.Attendee { FirstName = "Wes", LastName = "Gilleland", Username = "wes", Password = "b9c950640e1b3740e98acb93e669c65766f6670dd1609ba91ff41052ba48c6f3" },
-        //        new Models.Attendee { FirstName = "Melinda", LastName = "Cundiff", Username = "melinda64", Password = "35602208e86ac7d6b3a63780a9538a9d1763a646d5b9f3930a0548e0983e0ca6" },
-        //        new Models.Attendee { FirstName = "Jeremy", LastName = "Rice", Username = "talnius", Password = "4a969b0fb995b002a31f7f50742e6c83687fd9d67aa2c11f9eb88caaeb05c783" }
-        //     );
+            context.Attendees.AddOrUpdate(
+                a => a.Username,
+                new Models.Attendee { FirstName = "William", LastName = "Curry", Username = "wcc17", Password = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8" },
+                new Models.Attendee { FirstName = "Wes", LastName = "Gilleland", Username = "wes", Password = "b9c950640e1b3740e98acb93e669c65766f6670dd1609ba91ff41052ba48c6f3" },
+                new Models.Attendee { FirstName = "Melinda", LastName = "Cundiff", Username = "melinda64", Password = "35602208e86ac7d6b3a63780a9538a9d1763a646d5b9f3930a0548e0983e0ca6" },
+                new Models.Attendee { FirstName = "Jeremy", LastName = "Rice", Username = "talnius", Password = "4a969b0fb995b002a31f7f50742e6c83687fd9d67aa2c11f9eb88caaeb05c783" }
+             );
 
-        //    context.Rooms.AddOrUpdate(
-        //        r => r.RoomNo,
-        //        new Models.Room { Capacity = 20, RoomNo="WALL445"},
-        //        new Models.Room { Capacity = 25, RoomNo="WALL455"},
-        //        new Models.Room { Capacity = 23, RoomNo="WALL325"},
-        //        new Models.Room { Capacity = 30, RoomNo="COMB329"}
-        //    );
-        //    Attendee owner = context.Attendees.First(u => u.FirstName == "Wes");
-        //    Attendee[] attendees = new Attendee[]
-        //    {
-        //        context.Attendees.First(a => a.FirstName == "William"),
-        //        context.Attendees.First(a => a.FirstName == "Melinda")
-        //    };
-        //    context.Meetings.AddOrUpdate(
-        //        m => m.ID,
-        //        new Models.Meeting
-        //        {
-        //            Attendees = attendees,
-        //            Description = "This is a test meeting!",
-        //            EndTime = new DateTime(2016, 4, 30, 12, 30, 0),
-        //            StartTime = new DateTime(2016, 4, 30, 12, 0, 0),
-        //            HostingRoom = context.Rooms.First(r => r.RoomNo == "WALL445"),
-        //            Owner = owner,
-        //            Title = "Test Case: Please Ignore"
-        //        }
-        //     );
+            context.Rooms.AddOrUpdate(
+                r => r.RoomNo,
+                new Models.Room { Capacity = 20, RoomNo = "WALL445" },
+                new Models.Room { Capacity = 25, RoomNo = "WALL455" },
+                new Models.Room { Capacity = 23, RoomNo = "WALL325" },
+                new Models.Room { Capacity = 30, RoomNo = "COMB329" }
+            );
+
+            Attendee owner = context.Attendees.First(u => u.FirstName == "Wes");
+
+            Attendee[] attendees = new Attendee[]
+            {
+                    context.Attendees.First(a => a.FirstName == "William"),
+                    context.Attendees.First(a => a.FirstName == "Melinda")
+            };
+
+            context.Meetings.AddOrUpdate(
+                m => m.ID,
+                new Models.Meeting
+                {
+                    Attendees = attendees,
+                    Description = "This is a test meeting!",
+                    EndTime = new DateTime(2016, 4, 30, 12, 30, 0),
+                    StartTime = new DateTime(2016, 4, 30, 12, 0, 0),
+                    HostingRoom = context.Rooms.First(r => r.RoomNo == "WALL445"),
+                    Owner = owner,
+                    Title = "Test Case: Please Ignore"
+                }
+             );
+
+            if (System.Diagnostics.Debugger.IsAttached == false)
+                System.Diagnostics.Debugger.Launch();
+
         }
     }
 }
