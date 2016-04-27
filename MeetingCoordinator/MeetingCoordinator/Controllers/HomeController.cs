8b1d960f695eac2ee5704e5b5495987d9e2eb13a
wcc17 <christian.curry17@gmail.com>
2016-04-27 18:21:01 -0400

    Removed unecesary code and started on schedule meeting functionality

--- MeetingCoordinator/MeetingCoordinator/Controllers/HomeController.cs
+++ MeetingCoordinator/MeetingCoordinator/Controllers/HomeController.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using MeetingCoordinator.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace MeetingCoordinator.Controllers
 {
     public class HomeController : Controller
     {
-//        [Authorize]
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //        [Authorize]
         public ActionResult Index()
         {
             return View();
@@ -50,5 +54,50 @@ namespace MeetingCoordinator.Controllers
                 //exception encountered
             }
         }
+
+        [HttpPost]
+        public ActionResult CheckTimes()
+        {
+            String startDateTimeString;
+            String endDateTimeString;
+
+            startDateTimeString = Request.Form["start_time"];
+            endDateTimeString = Request.Form["end_time"];
+
+            DateTime startDateTime = DateTime.Parse(startDateTimeString);
+            DateTime endDateTime = DateTime.Parse(endDateTimeString);
+
+            List<Room> availableRooms = new List<Room>();
+            availableRooms.AddRange(db.Rooms);
+            foreach(Meeting m in db.Meetings)
+            {
+                /*
+                NOTE: this does not allow for overlap of meeting times, e.g.
+                if one meeting starts at 2:00 and ends at 3:00 and the next starts at 3:00,
+                then this will return a conflict. Is this the behavior we want?
+                */
+                if(startDateTime <= m.EndTime && m.StartTime <= endDateTime)
+                {
+                    if(availableRooms.Contains(m.HostingRoom))
+                    {
+                        availableRooms.Remove(m.HostingRoom);
+                    }
+                }
+            }
+
+            var serializer = new JavaScriptSerializer();
+            var list = serializer.Serialize(availableRooms);
+            JsonResult result = Json(new
+            {
+                status = true,
+                data = new
+                {
+                    rooms = list
+                }
+            });
+
+            return result;
+        }
+
     }
 }
