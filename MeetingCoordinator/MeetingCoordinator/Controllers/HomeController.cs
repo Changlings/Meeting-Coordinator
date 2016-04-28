918bfe60c0fe9d2010f15524d7ef82ae7d7358f4
Wes Gilleland <wes.gilleland@gmail.com>
2016-04-27 20:49:14 -0400

    Meetings pulling from database and populating properly in list view on Index

--- MeetingCoordinator/MeetingCoordinator/Controllers/HomeController.cs
+++ MeetingCoordinator/MeetingCoordinator/Controllers/HomeController.cs
@@ -1,37 +1,73 @@
 ﻿using MeetingCoordinator.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using System.Web.Script.Serialization;
+using Microsoft.Ajax.Utilities;
 
 namespace MeetingCoordinator.Controllers
 {
     public class HomeController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
 
         //        [Authorize]
-        public ActionResult Index()
-        {
+        public ActionResult Index(int year = -1, int month = -1)
+        {    
+//            System.Diagnostics.Debugger.Launch();
+//            int attendeeId = int.Parse(User.Identity.GetUserId());
+            var currentAttendee = _db.Attendees.First(a => a.FirstName == "Wes");
+            year = year == -1 ? DateTime.Now.Year : year;
+            month = month == -1 ? DateTime.Now.Month : month;
+            var thisMonthFirstDay = new DateTime(year, month, 1);
+            var nextMonthFirstDay = thisMonthFirstDay.AddMonths(1);
+            var ownMeetings = currentAttendee.OwnMeetings.Where(m => m.StartTime >= thisMonthFirstDay && m.EndTime < nextMonthFirstDay).OrderBy(m => m.StartTime).ToList();
+            var attendingMeetings = currentAttendee.AttendingMeetings.Where(m => m.StartTime >= thisMonthFirstDay && m.EndTime < nextMonthFirstDay).OrderBy(m => m.StartTime).ToList();
+            ViewBag.meetings = ownMeetings.Union(attendingMeetings).ToList();
+            ViewBag.ownMeetings = ownMeetings;
+            ViewBag.attendingMeetings = attendingMeetings;
             return View();
         }
 
-//        [Authorize]
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult EditMeeting(int id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var meetingResult = _db.Meetings.First(meeting => meeting.ID == id);
+            if (meetingResult == null)
+            {
+                return Json(new {success = false, error = "No meeting with that ID found"});
+            }
+            return Json(meetingResult);
         }
 
-//        [Authorize]
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult EditMeetingSubmit()
         {
-            ViewBag.Message = "Your contact page.";
+            var meetingId = int.Parse(Request.Form.Get("meeting-id"));
+            var meeting = _db.Meetings.Find(meetingId);
 
-            return View();
+            if (meeting == null)
+            {
+                return Json(new { success = false, error = "No meeting with that ID found" });
+            }
+            var title = Request.Form.Get("title");
+            var description = Request.Form.Get("description");
+            var startTime = Request.Form.Get("start-time");
+            var endTime = Request.Form.Get("end-time");
+            var attendeeIds = Request.Form.Get("attendees").Split(',').Select(int.Parse).ToList();
+
+            meeting.Title = title;
+            meeting.Description = description;
+            meeting.EndTime = DateTime.Parse(endTime);
+            meeting.StartTime = DateTime.Parse(startTime);
+            meeting.Attendees = _db.Attendees.Where(a => attendeeIds.Contains(a.ID)).ToList();
+            _db.SaveChangesAsync();
+
+            return Json(new {success = true});
         }
 
         [HttpPost]
@@ -58,45 +94,22 @@ namespace MeetingCoordinator.Controllers
         [HttpPost]
         public ActionResult CheckTimes()
         {
-            String startDateTimeString;
-            String endDateTimeString;
+            var startDateTimeString = Request.Form["start_time"];
+            var endDateTimeString = Request.Form["end_time"];
 
-            startDateTimeString = Request.Form["start_time"];
-            endDateTimeString = Request.Form["end_time"];
+            var startDateTime = DateTime.Parse(startDateTimeString);
+            var endDateTime = DateTime.Parse(endDateTimeString);
 
-            DateTime startDateTime = DateTime.Parse(startDateTimeString);
-            DateTime endDateTime = DateTime.Parse(endDateTimeString);
-
-            List<Room> availableRooms = new List<Room>();
-            availableRooms.AddRange(db.Rooms);
-            foreach(Meeting m in db.Meetings)
+            var availableRooms = new List<Room>();
+            availableRooms.AddRange(_db.Rooms);
+            foreach (var m in _db.Meetings.Where(m => startDateTime <= m.EndTime && m.StartTime <= endDateTime).Where(m => availableRooms.Contains(m.HostingRoom)))
             {
-                /*
-                NOTE: this does not allow for overlap of meeting times, e.g.
-                if one meeting starts at 2:00 and ends at 3:00 and the next starts at 3:00,
-                then this will return a conflict. Is this the behavior we want?
-                */
-                if(startDateTime <= m.EndTime && m.StartTime <= endDateTime)
-                {
-                    if(availableRooms.Contains(m.HostingRoom))
-                    {
-                        availableRooms.Remove(m.HostingRoom);
-                    }
-                }
+                availableRooms.Remove(m.HostingRoom);
             }
 
             var serializer = new JavaScriptSerializer();
             var list = serializer.Serialize(availableRooms);
-            JsonResult result = Json(new
-            {
-                status = true,
-                data = new
-                {
-                    rooms = list
-                }
-            });
-
-            return result;
+            return Json(new {status = true, data = new { rooms = list } });
         }
 
     }
