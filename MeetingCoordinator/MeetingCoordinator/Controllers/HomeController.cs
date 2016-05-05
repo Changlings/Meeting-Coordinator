14d3ef6c7ff7d998b91de85707e7f4538a165865
Wes Gilleland <wes.gilleland@gmail.com>
2016-05-05 17:07:52 -0400

    Meetings now appear in calendar view

--- MeetingCoordinator/MeetingCoordinator/Controllers/HomeController.cs
+++ MeetingCoordinator/MeetingCoordinator/Controllers/HomeController.cs
@@ -12,218 +12,223 @@ using Newtonsoft.Json;
 
 namespace MeetingCoordinator.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
+    //        [Authorize]
+    public ActionResult Index(int year = -1, int month = -1)
     {
-        private readonly ApplicationDbContext _db = new ApplicationDbContext();
-
-        //        [Authorize]
-        public ActionResult Index(int year = -1, int month = -1)
-        {
-            //System.Diagnostics.Debugger.Launch();
-            //            int attendeeId = int.Parse(User.Identity.GetUserId());
-            // TODO: FIX THE DAMNED OWIN STUFF
-            var currentAttendee = _db.Attendees.First(a => a.FirstName == "Wes");
-            year = year == -1 ? DateTime.Now.Year : year;
-            month = month == -1 ? DateTime.Now.Month : month;
-            var thisMonthFirstDay = new DateTime(year, month, 1);
-            var nextMonthFirstDay = thisMonthFirstDay.AddMonths(1);
-            var ownMeetings = currentAttendee.OwnMeetings.Where(m => m.StartTime >= thisMonthFirstDay && m.EndTime < nextMonthFirstDay).OrderBy(m => m.StartTime).ToList();
-            var attendingMeetings = currentAttendee.AttendingMeetings.Where(m => m.StartTime >= thisMonthFirstDay && m.EndTime < nextMonthFirstDay).OrderBy(m => m.StartTime).ToList();
-            ViewBag.meetings = ownMeetings.Union(attendingMeetings).ToList();
-            ViewBag.ownMeetings = ownMeetings;
-            ViewBag.attendingMeetings = attendingMeetings;
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Meeting(int id)
-        {
-            var meetingResult = _db.Meetings.Find(id);
-            if (meetingResult == null)
-            {
-                return Json(new {success = false, error = "No meeting with that ID found"}, JsonRequestBehavior.AllowGet);
-            }
-            return Json(new
-            {
-                title = meetingResult.Title,
-                description = meetingResult.Description,
-                startTime = meetingResult.StartTime,
-                endTime = meetingResult.EndTime,
-                attendees = meetingResult.Attendees.Select(a => new {id = a.ID, firstName = a.FirstName, lastName = a.LastName}).ToList()
-            }, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpPost]
-        public ActionResult EditMeeting()
-        {
-            var meetingId = int.Parse(Request.Form.Get("meeting-id"));
-            var meeting = _db.Meetings.Find(meetingId);
-
-            if (meeting == null)
-            {
-                return Json(new { success = false, error = "No meeting with that ID found" });
-            }
-            var title = Request.Form.Get("title");
-            var description = Request.Form.Get("description");
-            var startTime = Request.Form.Get("start-time");
-            var endTime = Request.Form.Get("end-time");
-            var attendeeIds = Request.Form.Get("attendees").Split(',').Select(int.Parse).ToList();
-
-            meeting.Title = title;
-            meeting.Description = description;
-            meeting.EndTime = DateTime.Parse(endTime);
-            meeting.StartTime = DateTime.Parse(startTime);
-            meeting.Attendees = _db.Attendees.Where(a => attendeeIds.Contains(a.ID)).ToList();
-            _db.SaveChangesAsync();
-
-            return Json(new {success = true});
-        }
-
-        /*
-        *   Populate the room and attendee select forms with all of the possible rooms and attendees. 
-        */
-        [HttpGet]
-        public ActionResult GetSchedulingData()
-        {
-            var attendeesList = new List<Attendee>();
-            var roomsList = new List<Room>();
-
-            //TODO: remove the user actually creating the meeting from the list of attendees
-            attendeesList.AddRange(_db.Attendees);
-            roomsList.AddRange(_db.Rooms);
-
-            JsonResult result = Json(new
-            {
-                status = true,
-                attendees = attendeesList.Select(a => new
-                {
-                    ID = a.ID,
-                    FirstName = a.FirstName,
-                    LastName = a.LastName
-                }),
-
-                rooms = roomsList.Select(r => new
-                {
-                    ID = r.ID,
-                    RoomNo = r.RoomNo
-                })
-
-            }, JsonRequestBehavior.AllowGet);
-
-
-            return result;
-        }
-
-        /*
-        *   
-        */
-        [HttpPost]
-        public ActionResult CheckAvailability()
-        {
-            var errors = new List<String>();
-            bool meetingAvailable = true;
-
-            var startDateTimeString = Request.Form["start_time"];
-            var endDateTimeString = Request.Form["end_time"];
-            var selectedRoomID = Int32.Parse(Request.Form["room_id"]);
-
-            var attendeeString = Request.Form["attendees[]"];
-            var attendeeIDList = (
-                from string s in attendeeString.Split(',')
-                select Convert.ToInt32(s)
-            ).ToList<int>();
-            var attendeeList = new List<Attendee>();
-
-            foreach (int i in attendeeIDList)
-            {
-                Attendee a = _db.Attendees.Find(i);
-                attendeeList.Add(a);
-            }
-
-            var startDateTime = DateTime.Parse(startDateTimeString);
-            var endDateTime = DateTime.Parse(endDateTimeString);
-
-            //if an existing meeting is already using the selected room at the selected time
-            Room room = _db.Rooms.First(r => r.ID == selectedRoomID);
-            foreach (var m in _db.Meetings.Where(m => startDateTime <= m.EndTime && m.StartTime <= endDateTime).Where(m => selectedRoomID == m.HostingRoom.ID))
-            {
-                errors.Add("Room " + room.RoomNo + " is already in use during this time.");
-                meetingAvailable = false;
-            }
-
-            //TODO: check if owner of this meeting is already owner of another meeting or attending another meeting during this time
-            //if an attendee is already attending a meeting during the selected time
-            var overlappingMeetings = _db.Meetings.Where(m => startDateTime <= m.EndTime && m.StartTime <= endDateTime).ToList();
-            foreach (Meeting m in overlappingMeetings)
-            {
-                if(attendeeList.Count() > 0)
-                {
-                    //get the attendees that exist both in the selected attendees list and the attendees that are attending a meeting happening during the selected times
-                    var overlappingAttendees = m.Attendees.Intersect(attendeeList).ToList();
-                    foreach (var a in overlappingAttendees)
-                    {
-                        errors.Add(a.FirstName + " " + a.LastName + " is already attending a meeting during this time.");
-                        attendeeList.Remove(a); //so this error won't be encountered again
-                        meetingAvailable = false;
-                    }
-
-                }
-            }
-
-            if(endDateTime <= startDateTime)
-            {
-                errors.Add("End date and time must be after start date and time.");
-                meetingAvailable = false;
-            }
-
-            return Json(new
-            {
-                status = errors.Count == 0,
-                meetingAvailable = meetingAvailable,
-                errors = Json(errors)
-            });
-        }
-
-        [HttpPost]
-        public ActionResult SaveMeeting()
-        {
-            Boolean saveSuccessful = false;
-
-            var title = Request.Form.Get("title");
-            var description = Request.Form.Get("description");
-            var roomId = Int32.Parse(Request.Form.Get("room_id"));
-
-            var attendeeIds = Request.Form.Get("attendee_ids[]");
-            var attendeeIdList = (
-                from string s in attendeeIds.Split(',')
-                select Convert.ToInt32(s)
-            ).ToList<int>();
-
-            var startTime = Request.Form.Get("start_time");
-            var endTime = Request.Form.Get("end_time");
-
-            var meeting = new Meeting();
-            meeting.Title = title;
-            meeting.Description = description;
-            meeting.EndTime = DateTime.Parse(endTime);
-            meeting.StartTime = DateTime.Parse(startTime);
-            meeting.Attendees = _db.Attendees.Where(a => attendeeIdList.Contains(a.ID)).ToList();
-            meeting.HostingRoom = _db.Rooms.First(r => r.ID == roomId);
-
-            //TODO: GET RID OF HARDCODED OWNER ONCE OWIN STUFF IS FIXED
-            meeting.Owner = _db.Attendees.First(a => a.FirstName == "William");
-
-            try
-            {
-                _db.Meetings.Add(meeting);
-                _db.SaveChangesAsync();
-            }
-            catch (Exception)
-            {
-                saveSuccessful = false;
-            }
-
-            return Json(new { success = saveSuccessful });
-        }
+      //System.Diagnostics.Debugger.Launch();
+      //            int attendeeId = int.Parse(User.Identity.GetUserId());
+      // TODO: FIX THE DAMNED OWIN STUFF
+      var currentAttendee = _db.Attendees.First(a => a.FirstName == "Wes");
+      year = year == -1 ? DateTime.Now.Year : year;
+      month = month == -1 ? DateTime.Now.Month : month;
+      var thisMonthFirstDay = new DateTime(year, month, 1);
+      var nextMonthFirstDay = thisMonthFirstDay.AddMonths(1);
+      var ownMeetings = currentAttendee.OwnMeetings.Where(m => m.StartTime >= thisMonthFirstDay && m.EndTime < nextMonthFirstDay).OrderBy(m => m.StartTime).ToList();
+      var attendingMeetings = currentAttendee.AttendingMeetings.Where(m => m.StartTime >= thisMonthFirstDay && m.EndTime < nextMonthFirstDay).OrderBy(m => m.StartTime).ToList();
+      ViewBag.meetings = ownMeetings.Union(attendingMeetings).ToList();
+      ViewBag.ownMeetings = ownMeetings;
+      ViewBag.attendingMeetings = attendingMeetings;
+      return View();
     }
+
+    [HttpGet]
+    public ActionResult Meeting(int id)
+    {
+      var meetingResult = _db.Meetings.Find(id);
+      if (meetingResult == null)
+      {
+        return Json(new { success = false, error = "No meeting with that ID found" }, JsonRequestBehavior.AllowGet);
+      }
+      return Json(new
+      {
+        title = meetingResult.Title,
+        description = meetingResult.Description,
+        startTime = meetingResult.StartTime,
+        endTime = meetingResult.EndTime,
+        attendees = meetingResult.Attendees.Select(a => new { id = a.ID, firstName = a.FirstName, lastName = a.LastName }).ToList()
+      }, JsonRequestBehavior.AllowGet);
+    }
+
+    [HttpPost]
+    public ActionResult EditMeeting()
+    {
+      var meetingId = int.Parse(Request.Form.Get("meeting-id"));
+      var meeting = _db.Meetings.Find(meetingId);
+
+      if (meeting == null)
+      {
+        return Json(new { success = false, error = "No meeting with that ID found" });
+      }
+      var title = Request.Form.Get("title");
+      var description = Request.Form.Get("description");
+      var startTime = Request.Form.Get("start-time");
+      var endTime = Request.Form.Get("end-time");
+      var attendeeIds = Request.Form.Get("attendees").Split(',').Select(int.Parse).ToList();
+
+      meeting.Title = title;
+      meeting.Description = description;
+      meeting.EndTime = DateTime.Parse(endTime);
+      meeting.StartTime = DateTime.Parse(startTime);
+      meeting.Attendees = _db.Attendees.Where(a => attendeeIds.Contains(a.ID)).ToList();
+      _db.SaveChangesAsync();
+
+      return Json(new { success = true });
+    }
+
+    /*
+    *   Populate the room and attendee select forms with all of the possible rooms and attendees. 
+    */
+    [HttpGet]
+    public ActionResult GetSchedulingData()
+    {
+      var attendeesList = new List<Attendee>();
+      var roomsList = new List<Room>();
+
+      //TODO: remove the user actually creating the meeting from the list of attendees
+      attendeesList.AddRange(_db.Attendees);
+      roomsList.AddRange(_db.Rooms);
+
+      JsonResult result = Json(new
+      {
+        status = true,
+        attendees = attendeesList.Select(a => new
+        {
+          ID = a.ID,
+          FirstName = a.FirstName,
+          LastName = a.LastName
+        }),
+
+        rooms = roomsList.Select(r => new
+        {
+          ID = r.ID,
+          RoomNo = r.RoomNo
+        })
+
+      }, JsonRequestBehavior.AllowGet);
+
+
+      return result;
+    }
+
+    /*
+    *   
+    */
+    [HttpPost]
+    public ActionResult CheckAvailability()
+    {
+      var errors = new List<String>();
+      bool meetingAvailable = true;
+
+      var startDateTimeString = Request.Form["start_time"];
+      var endDateTimeString = Request.Form["end_time"];
+      var selectedRoomID = Int32.Parse(Request.Form["room_id"]);
+
+      var attendeeString = Request.Form["attendees[]"];
+      var attendeeIDList = (
+          from string s in attendeeString.Split(',')
+          select Convert.ToInt32(s)
+      ).ToList<int>();
+      var attendeeList = new List<Attendee>();
+
+      foreach (int i in attendeeIDList)
+      {
+        Attendee a = _db.Attendees.Find(i);
+        attendeeList.Add(a);
+      }
+
+      var startDateTime = DateTime.Parse(startDateTimeString);
+      var endDateTime = DateTime.Parse(endDateTimeString);
+
+      //if an existing meeting is already using the selected room at the selected time
+      Room room = _db.Rooms.First(r => r.ID == selectedRoomID);
+      foreach (var m in _db.Meetings.Where(m => startDateTime <= m.EndTime && m.StartTime <= endDateTime).Where(m => selectedRoomID == m.HostingRoom.ID))
+      {
+        errors.Add("Room " + room.RoomNo + " is already in use during this time.");
+        meetingAvailable = false;
+      }
+
+      //TODO: check if owner of this meeting is already owner of another meeting or attending another meeting during this time
+      //if an attendee is already attending a meeting during the selected time
+      var overlappingMeetings = _db.Meetings.Where(m => startDateTime <= m.EndTime && m.StartTime <= endDateTime).ToList();
+      foreach (Meeting m in overlappingMeetings)
+      {
+        if (attendeeList.Count() > 0)
+        {
+          //get the attendees that exist both in the selected attendees list and the attendees that are attending a meeting happening during the selected times
+          var overlappingAttendees = m.Attendees.Intersect(attendeeList).ToList();
+          foreach (var a in overlappingAttendees)
+          {
+            errors.Add(a.FirstName + " " + a.LastName + " is already attending a meeting during this time.");
+            attendeeList.Remove(a); //so this error won't be encountered again
+            meetingAvailable = false;
+          }
+
+        }
+      }
+
+      if (endDateTime <= startDateTime)
+      {
+        errors.Add("End date and time must be after start date and time.");
+        meetingAvailable = false;
+      }
+
+      return Json(new
+      {
+        status = errors.Count == 0,
+        meetingAvailable = meetingAvailable,
+        errors = Json(errors)
+      });
+    }
+
+    [HttpPost]
+    public ActionResult SaveMeeting()
+    {
+      Boolean saveSuccessful = false;
+
+      var title = Request.Form.Get("title");
+      var description = Request.Form.Get("description");
+      var roomId = Int32.Parse(Request.Form.Get("room_id"));
+
+      var attendeeIds = Request.Form.Get("attendee_ids[]");
+      var attendeeIdList = (
+          from string s in attendeeIds.Split(',')
+          select Convert.ToInt32(s)
+      ).ToList<int>();
+
+      var startTime = Request.Form.Get("start_time");
+      var endTime = Request.Form.Get("end_time");
+
+      var meeting = new Meeting();
+      meeting.Title = title;
+      meeting.Description = description;
+      meeting.EndTime = DateTime.Parse(endTime);
+      meeting.StartTime = DateTime.Parse(startTime);
+      meeting.Attendees = _db.Attendees.Where(a => attendeeIdList.Contains(a.ID)).ToList();
+      meeting.HostingRoom = _db.Rooms.First(r => r.ID == roomId);
+
+      //TODO: GET RID OF HARDCODED OWNER ONCE OWIN STUFF IS FIXED
+      meeting.Owner = _db.Attendees.First(a => a.FirstName == "William");
+
+      try
+      {
+        _db.Meetings.Add(meeting);
+        _db.SaveChanges();
+        return Json(new {success = true, meeting = new
+        {
+          id = meeting.ID,
+          title = meeting.Title,
+          start = meeting.StartTime,
+          end = meeting.EndTime
+        }});
+      }
+      catch (Exception e)
+      {
+        return Json(new {success = false, error = e.Message});
+      }
+    }
+  }
 }
--- MeetingCoordinator/MeetingCoordinator/Scripts/js/main.js
+++ MeetingCoordinator/MeetingCoordinator/Scripts/js/main.js
@@ -1,22 +1,72 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function() {
+  /* 
+   * initialize the calendar
+   */
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    editable: true
+  });
+  $('#external-events div.external-event').each(function () {
+    // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+    // it doesn't need to have a start or end
+    debugger;
+    var data = $(this).find('span.meeting').get(0);
+    var eventObject = {
+      id: data.dataset["id"],
+      title: $.trim($(this).text()),
+      start: data.dataset["start"],
+      end: data.dataset["end"]
+    };
+    $('#calendar').fullCalendar('renderEvent', eventObject);
+  });
+  /* Hide Default header : coz our bottons look awesome */
+  $('.fc-header').hide();
+  //Get the current date and display on the tile
+  var currentDate = $('#calendar').fullCalendar('getDate');
+  $('#calender-current-day').html($.fullCalendar.formatDate(currentDate, "dddd"));
+  $('#calender-current-date').html($.fullCalendar.formatDate(currentDate, "MMM yyyy"));
+  $('#calender-prev').click(function() {
+    $('#calendar').fullCalendar('prev');
+    currentDate = $('#calendar').fullCalendar('getDate');
+    $('#calender-current-day').html($.fullCalendar.formatDate(currentDate, "dddd"));
+    $('#calender-current-date').html($.fullCalendar.formatDate(currentDate, "MMM yyyy"));
+  });
+  $('#calender-next').click(function() {
+    $('#calendar').fullCalendar('next');
+    currentDate = $('#calendar').fullCalendar('getDate');
+    $('#calender-current-day').html($.fullCalendar.formatDate(currentDate, "dddd"));
+    $('#calender-current-date').html($.fullCalendar.formatDate(currentDate, "MMM yyyy"));
+  });
+  $('#change-view-month').click(function() {
+    $('#calendar').fullCalendar('changeView', 'month');
+  });
+  $('#change-view-week').click(function() {
+    $('#calendar').fullCalendar('changeView', 'agendaWeek');
+  });
+  $('#change-view-day').click(function() {
+    $('#calendar').fullCalendar('changeView', 'agendaDay');
+  });
   /**
    * Event listener for handling getting data about a meeting from the server and
    * displaying the Edit Meeting modal
    */
-  $('.edit-meeting').click(function (e) {
+  $('.edit-meeting').click(function(e) {
     // TODO: SERVER LOGIC (EDIT MEETING USE CASE)
     $('.modal-body').find('input[name=title]').val(this.dataset.title);
-    $(this).closest('.external-event').find('.meeting')[0].dataset.attendees.split(',').forEach(function (val, index) {
+    $(this).closest('.external-event').find('.meeting')[0].dataset.attendees.split(',').forEach(function(val, index) {
       $('select[name=attendees]').find('option[value=' + val + ']').attr('selected', true);
     });
     $('#delete-meeting')[0].dataset.meetingId = this.dataset.id;
     $('#meeting-edit').modal("show");
   });
-
   /**
    * Event listener for deleting a meeting from an Edit Meeting modal
    */
-  $('#delete-meeting').click(function (e) {
+  $('#delete-meeting').click(function(e) {
     if (confirm("Are you sure you wish to remove this meeting? This cannot be undone!")) {
       // TODO: SERVER LOGIC (EDIT-MEETING USE CASE)
       // Remove the list item on the main page
@@ -25,33 +75,32 @@
       $('#meeting-edit').modal("hide");
     }
   });
-
   /**
    * Event listener for deleting a meeting from the list view
    */
-  $('.delete-meeting').click(function (e) {
+  $('.delete-meeting').click(function(e) {
     if (confirm("Are you sure you wish to remove this meeting? This cannot be undone!")) {
-      // TODO: SERVER LOGIC (REMOVE MEETING USE CASE)
-      $(this).closest('.external-event').remove();
+      $(this).closest(".external-event").remove();
     }
   });
-
   /**
    * Event listener for clicking on a meeting link in the list view
    */
   $(document).on({
-    click: function () {
+    click: function() {
       // Get the ID of this meeting from the data-id attribute
       var id = this.dataset['id'];
       // Fire off a JSON request to get the meeting according to the id
-      $.getJSON("/Home/Meeting", { id: id }, function (response) {
+      $.getJSON("/Home/Meeting", {
+        id: id
+      }, function(response) {
         // empty the modal of previous results
-        $.each($('.meeting-detail'), function (index, element) {
+        $.each($('.meeting-detail'), function(index, element) {
           $(element).empty();
         });
         // We need to filter out the attributes we need for Attendees
         var attendees = [];
-        response.attendees.forEach(function (attendee) {
+        response.attendees.forEach(function(attendee) {
           attendees.push(attendee.firstName + " " + attendee.lastName);
         });
         // Update the details for the modal
@@ -59,34 +108,30 @@
         $('#detail-title').text(response.title);
         $('#detail-description').text(response.description);
         // Use moment.js to parse the times given back by the server into a nice message
-        $('#detail-time').text(moment(parseInt(/-?\d+/.exec(response.startTime))).format("MMMM Do YYYY, h:mm:ss a") +
-            " to " + moment(parseInt(/-?\d+/.exec(response.endTime))).format("MMMM Do YYYY, h:mm:ss a"));
+        $('#detail-time').text(moment(parseInt(/-?\d+/.exec(response.startTime))).format("MMMM Do YYYY, h:mm:ss a") + " to " + moment(parseInt(/-?\d+/.exec(response.endTime))).format("MMMM Do YYYY, h:mm:ss a"));
         // Show the modal
         $('#meeting-details').modal('show');
       });
     }
   }, ".external-event > span.meeting, .external-event > span.edit-meeting");
-
-  $('#schedule-meeting-sidebar').click(function (e) {
+  $('#schedule-meeting-sidebar').click(function(e) {
     var $meeting = $('#meeting-create');
-    $meeting.find('input, textarea').each(function (index, element) {
+    $meeting.find('input, textarea').each(function(index, element) {
       $(element).val("");
     });
-    $meeting.find('option').each(function (index, element) {
+    $meeting.find('option').each(function(index, element) {
       $(element).attr("selected", false);
     });
     $meeting.modal("show");
-
     //get list of attendees and list of rooms from controller
     $.ajax({
       type: "GET",
       dataType: "json",
       url: "/Home/GetSchedulingData",
       async: true,
-      success: function (data, status) {
+      success: function(data, status) {
         var rooms = data['rooms'];
         var attendees = data['attendees'];
-
         if (rooms) {
           var meeting_room_select = $('#meeting-create').find('select[name=meeting-room]');
           meeting_room_select.empty();
@@ -104,31 +149,25 @@
       }
     })
   });
-
   //after all info is filled in, check if the meeting is valid
   $('#start-time-input, #end-time-input').on({
-    change: function (event) {
+    change: function(event) {
       var start_time = $('#start-time-input').val();
       var end_time = $('#end-time-input').val();
-
       //if both start and end time have been filled out
       if (start_time !== "" && end_time !== "") {
-
         var roomID = $('#meeting-create').find('select[name=meeting-room]').val();
         //TODO: change this to attendee_ids
         var attendees = [];
-
         //get all selected attendees
-        $('#attendees-multiple-select :selected').each(function (i, selected) {
+        $('#attendees-multiple-select :selected').each(function(i, selected) {
           attendees[i] = $(selected).val();
         });
-
         //if a room has been chosen and at least one attendee has been chosen 
         if (roomID != "" && attendees.length > 0) {
           //reformat start and end time
           start_time = start_time.replace("T", " ");
           end_time = end_time.replace("T", " ");
-
           //send filled in data back to controller
           $.ajax({
             data: {
@@ -141,7 +180,7 @@
             type: "POST",
             async: true,
             dataType: "json",
-            success: function (data, status) {
+            success: function(data, status) {
               if (status) {
                 if (!data.meetingAvailable) {
                   for (var i = 0; i < data.errors.Data.length; i++) {
@@ -151,26 +190,22 @@
                 }
               }
             }
-          })
+          });
         }
       }
     }
-  })
-
-  $('#save-new-meeting').click(function (e) {
+  });
+  $('#save-new-meeting').click(function(e) {
     var title = $('#meeting-create').find('input[name=title]').val();
     var description = $('#meeting-create').find('textarea[name=description]').val();
     var room_id = $('#meeting-create').find('select[name=meeting-room]').val();
     var attendee_ids = [];
-
     //get all selected attendee ids
-    $('#attendees-multiple-select :selected').each(function (i, selected) {
+    $('#attendees-multiple-select :selected').each(function(i, selected) {
       attendee_ids[i] = $(selected).val();
     });
-
     var start_time = $('#start-time-input').val();
     var end_time = $('#end-time-input').val();
-
     $.ajax({
       data: {
         title: title,
@@ -184,29 +219,24 @@
       dataType: "json",
       url: "/Home/SaveMeeting",
       async: true,
-      success: function (success) {
-        if (success) {
+      success: function(response) {
+        if (response.success) {
           alert("Meeting created!");
-          var meeting_id = $('.external-event').length + 1;
-          $('#external-events').append('<div class="external-event">' +
-                  '<span class="meeting" data-title="Meeting ' + meeting_id + '" data-attendees="" data-id="' + meeting_id + '">Meeting ' + meeting_id + '</span>' +
-                  '<span class="delete-meeting pull-right"><i class="fa fa-pencil"></i><i class="fa fa-times"></i></span>' +
-                  '</div>');
-
+          $('#external-events').append('<div class="external-event">' + '<span class="meeting" data-title="' + title + '" data-attendees="' + attendee_ids.join(",") + '" data-id="' + response.meeting.id + '">' + title + '</span>' + '<span class="pull-right">' + '<i class="edit-meeting fa fa-pencil"></i>' + '<i style="padding-left: 5px; padding-right: 5px;"></i>' + '<i class="delete-meeting fa fa-times"></i>' + '</span>');
+          $('#calendar').fullCalendar('renderEvent', response.meeting);
           $('#meeting-create').modal('hide');
         } else {
           //TODO: show an actual error message and handle it
           console.log("Error! fix this");
         }
       }
-    })
+    });
   });
 
-  $('#save-meeting-edit').click(function (e) {
+  $('#save-meeting-edit').click(function(e) {
     $.ajax({
       data: $('#meeting-edit').find('form').serialize(),
       url: "/Home/Edit"
-    })
+    });
   });
-
 });
