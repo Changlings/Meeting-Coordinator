888408456fbeaa9c09b023b577a684a95a3a4ec9
Wes Gilleland <wes.gilleland@gmail.com>
2016-05-05 16:03:38 -0400

    Re-organized the JS to its own file and the modals to their own partials

--- MeetingCoordinator/MeetingCoordinator/Models/DbContext.cs
+++ MeetingCoordinator/MeetingCoordinator/Models/DbContext.cs
@@ -7,35 +7,38 @@ using MySql.Data.Entity;
 
 namespace MeetingCoordinator.Models
 {
-    [DbConfigurationType(typeof(MySqlEFConfiguration))]
-    public class ApplicationDbContext : DbContext
+  [DbConfigurationType(typeof(MySqlEFConfiguration))]
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext() : base("DefaultConnection")
     {
-        public ApplicationDbContext() : base("DefaultConnection") { }
-        public DbSet<Meeting> Meetings { get; set; }
-        public DbSet<Room> Rooms { get; set; }
-        public DbSet<Attendee> Attendees { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Properties<String>().Configure(c => c.HasColumnType("longtext"));
-
-            //each meeting has multiple attendees and each attendee has multiple meetings
-            modelBuilder.Entity<Attendee>()
-                .HasMany<Meeting>(a => a.AttendingMeetings)
-                .WithMany(m => m.Attendees)
-                .Map(mc =>
-                {
-                    mc.ToTable("AttendeeJoinMeeting");
-                    mc.MapLeftKey("AttendeeId");
-                    mc.MapRightKey("MeetingId");
-                });
-
-            //each meeting has one owner. but each attendee can own multiple meetings
-            modelBuilder.Entity<Meeting>()
-                .HasRequired<Attendee>(m => m.Owner)
-                .WithMany(a => a.OwnMeetings);
-
-            base.OnModelCreating(modelBuilder);
-        }
     }
+
+    public DbSet<Meeting> Meetings { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<Attendee> Attendees { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+      modelBuilder.Properties<String>().Configure(c => c.HasColumnType("longtext"));
+
+      //each meeting has multiple attendees and each attendee has multiple meetings
+      modelBuilder.Entity<Attendee>()
+        .HasMany<Meeting>(a => a.AttendingMeetings)
+        .WithMany(m => m.Attendees)
+        .Map(mc =>
+        {
+          mc.ToTable("AttendeeJoinMeeting");
+          mc.MapLeftKey("AttendeeId");
+          mc.MapRightKey("MeetingId");
+        });
+
+      //each meeting has one owner. but each attendee can own multiple meetings
+      modelBuilder.Entity<Meeting>()
+        .HasRequired<Attendee>(m => m.Owner)
+        .WithMany(a => a.OwnMeetings);
+
+      base.OnModelCreating(modelBuilder);
+    }
+  }
 }
---
+++ MeetingCoordinator/MeetingCoordinator/Scripts/js/main.js
@@ -0,0 +1,212 @@
+﻿$(document).ready(function () {
+  /**
+   * Event listener for handling getting data about a meeting from the server and
+   * displaying the Edit Meeting modal
+   */
+  $('.edit-meeting').click(function (e) {
+    // TODO: SERVER LOGIC (EDIT MEETING USE CASE)
+    $('.modal-body').find('input[name=title]').val(this.dataset.title);
+    $(this).closest('.external-event').find('.meeting')[0].dataset.attendees.split(',').forEach(function (val, index) {
+      $('select[name=attendees]').find('option[value=' + val + ']').attr('selected', true);
+    });
+    $('#delete-meeting')[0].dataset.meetingId = this.dataset.id;
+    $('#meeting-edit').modal("show");
+  });
+
+  /**
+   * Event listener for deleting a meeting from an Edit Meeting modal
+   */
+  $('#delete-meeting').click(function (e) {
+    if (confirm("Are you sure you wish to remove this meeting? This cannot be undone!")) {
+      // TODO: SERVER LOGIC (EDIT-MEETING USE CASE)
+      // Remove the list item on the main page
+      $('.meeting[data-id=' + this.dataset.meetingId + ']').closest('.external-event').remove();
+      // Hide the modal
+      $('#meeting-edit').modal("hide");
+    }
+  });
+
+  /**
+   * Event listener for deleting a meeting from the list view
+   */
+  $('.delete-meeting').click(function (e) {
+    if (confirm("Are you sure you wish to remove this meeting? This cannot be undone!")) {
+      // TODO: SERVER LOGIC (REMOVE MEETING USE CASE)
+      $(this).closest('.external-event').remove();
+    }
+  });
+
+  /**
+   * Event listener for clicking on a meeting link in the list view
+   */
+  $(document).on({
+    click: function () {
+      // Get the ID of this meeting from the data-id attribute
+      var id = this.dataset['id'];
+      // Fire off a JSON request to get the meeting according to the id
+      $.getJSON("/Home/Meeting", { id: id }, function (response) {
+        // empty the modal of previous results
+        $.each($('.meeting-detail'), function (index, element) {
+          $(element).empty();
+        });
+        // We need to filter out the attributes we need for Attendees
+        var attendees = [];
+        response.attendees.forEach(function (attendee) {
+          attendees.push(attendee.firstName + " " + attendee.lastName);
+        });
+        // Update the details for the modal
+        $('#detail-attendees').text(attendees.join(", "));
+        $('#detail-title').text(response.title);
+        $('#detail-description').text(response.description);
+        // Use moment.js to parse the times given back by the server into a nice message
+        $('#detail-time').text(moment(parseInt(/-?\d+/.exec(response.startTime))).format("MMMM Do YYYY, h:mm:ss a") +
+            " to " + moment(parseInt(/-?\d+/.exec(response.endTime))).format("MMMM Do YYYY, h:mm:ss a"));
+        // Show the modal
+        $('#meeting-details').modal('show');
+      });
+    }
+  }, ".external-event > span.meeting, .external-event > span.edit-meeting");
+
+  $('#schedule-meeting-sidebar').click(function (e) {
+    var $meeting = $('#meeting-create');
+    $meeting.find('input, textarea').each(function (index, element) {
+      $(element).val("");
+    });
+    $meeting.find('option').each(function (index, element) {
+      $(element).attr("selected", false);
+    });
+    $meeting.modal("show");
+
+    //get list of attendees and list of rooms from controller
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: "/Home/GetSchedulingData",
+      async: true,
+      success: function (data, status) {
+        var rooms = data['rooms'];
+        var attendees = data['attendees'];
+
+        if (rooms) {
+          var meeting_room_select = $('#meeting-create').find('select[name=meeting-room]');
+          meeting_room_select.empty();
+          for (var i = 0; i < rooms.length; i++) {
+            meeting_room_select.append('<option value=' + rooms[i].ID + '>' + rooms[i].RoomNo + '</option>');
+          }
+        }
+        if (attendees) {
+          var attendee_select = $('#meeting-create').find('select[name=attendees]');
+          attendee_select.empty();
+          for (var i = 0; i < attendees.length; i++) {
+            attendee_select.append('<option value=' + attendees[i].ID + '>' + attendees[i].FirstName + ' ' + attendees[i].LastName + '</option>');
+          }
+        }
+      }
+    })
+  });
+
+  //after all info is filled in, check if the meeting is valid
+  $('#start-time-input, #end-time-input').on({
+    change: function (event) {
+      var start_time = $('#start-time-input').val();
+      var end_time = $('#end-time-input').val();
+
+      //if both start and end time have been filled out
+      if (start_time !== "" && end_time !== "") {
+
+        var roomID = $('#meeting-create').find('select[name=meeting-room]').val();
+        //TODO: change this to attendee_ids
+        var attendees = [];
+
+        //get all selected attendees
+        $('#attendees-multiple-select :selected').each(function (i, selected) {
+          attendees[i] = $(selected).val();
+        });
+
+        //if a room has been chosen and at least one attendee has been chosen 
+        if (roomID != "" && attendees.length > 0) {
+          //reformat start and end time
+          start_time = start_time.replace("T", " ");
+          end_time = end_time.replace("T", " ");
+
+          //send filled in data back to controller
+          $.ajax({
+            data: {
+              end_time: end_time,
+              start_time: start_time,
+              room_id: roomID,
+              attendees: attendees
+            },
+            url: "/Home/CheckAvailability",
+            type: "POST",
+            async: true,
+            dataType: "json",
+            success: function (data, status) {
+              if (status) {
+                if (!data.meetingAvailable) {
+                  for (var i = 0; i < data.errors.Data.length; i++) {
+                    //TODO: show actual error messages to the user
+                    console.log(data.errors.Data[i]);
+                  }
+                }
+              }
+            }
+          })
+        }
+      }
+    }
+  })
+
+  $('#save-new-meeting').click(function (e) {
+    var title = $('#meeting-create').find('input[name=title]').val();
+    var description = $('#meeting-create').find('textarea[name=description]').val();
+    var room_id = $('#meeting-create').find('select[name=meeting-room]').val();
+    var attendee_ids = [];
+
+    //get all selected attendee ids
+    $('#attendees-multiple-select :selected').each(function (i, selected) {
+      attendee_ids[i] = $(selected).val();
+    });
+
+    var start_time = $('#start-time-input').val();
+    var end_time = $('#end-time-input').val();
+
+    $.ajax({
+      data: {
+        title: title,
+        description: description,
+        room_id: room_id,
+        attendee_ids: attendee_ids,
+        start_time: start_time,
+        end_time: end_time
+      },
+      type: "POST",
+      dataType: "json",
+      url: "/Home/SaveMeeting",
+      async: true,
+      success: function (success) {
+        if (success) {
+          alert("Meeting created!");
+          var meeting_id = $('.external-event').length + 1;
+          $('#external-events').append('<div class="external-event">' +
+                  '<span class="meeting" data-title="Meeting ' + meeting_id + '" data-attendees="" data-id="' + meeting_id + '">Meeting ' + meeting_id + '</span>' +
+                  '<span class="delete-meeting pull-right"><i class="fa fa-pencil"></i><i class="fa fa-times"></i></span>' +
+                  '</div>');
+
+          $('#meeting-create').modal('hide');
+        } else {
+          //TODO: show an actual error message and handle it
+          console.log("Error! fix this");
+        }
+      }
+    })
+  });
+
+  $('#save-meeting-edit').click(function (e) {
+    $.ajax({
+      data: $('#meeting-edit').find('form').serialize(),
+      url: "/Home/Edit"
+    })
+  });
+
+});
