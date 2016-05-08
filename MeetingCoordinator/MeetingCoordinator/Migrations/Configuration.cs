06bc5cb07ed2d0647dd983f90a256d549340fff2
wcc17 <christian.curry17@gmail.com>
2016-05-08 19:52:12 -0400

    added a User for Dr. Chang to the seed method

--- MeetingCoordinator/MeetingCoordinator/Migrations/Configuration.cs
+++ MeetingCoordinator/MeetingCoordinator/Migrations/Configuration.cs
@@ -36,12 +36,14 @@ namespace MeetingCoordinator.Migrations
             //wes - password1234
             //melinda64 - hotdog
             //talnius - hotdog1234
+            //drchang - theteacher
             context.Attendees.AddOrUpdate(
                 a => a.Username,
                 new Models.Attendee { FirstName = "William", LastName = "Curry", Username = "wcc17", Password = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8" },
                 new Models.Attendee { FirstName = "Wes", LastName = "Gilleland", Username = "wes", Password = "b9c950640e1b3740e98acb93e669c65766f6670dd1609ba91ff41052ba48c6f3" },
                 new Models.Attendee { FirstName = "Melinda", LastName = "Cundiff", Username = "melinda64", Password = "35602208e86ac7d6b3a63780a9538a9d1763a646d5b9f3930a0548e0983e0ca6" },
-                new Models.Attendee { FirstName = "Jeremy", LastName = "Rice", Username = "talnius", Password = "4a969b0fb995b002a31f7f50742e6c83687fd9d67aa2c11f9eb88caaeb05c783" }
+                new Models.Attendee { FirstName = "Jeremy", LastName = "Rice", Username = "talnius", Password = "4a969b0fb995b002a31f7f50742e6c83687fd9d67aa2c11f9eb88caaeb05c783" },
+                new Models.Attendee { FirstName = "Kuangnan", LastName = "Chang", Username = "drchang", Password = "9e251b9b45ffa7a700e62665b09e833582045bbbee4f069527fae2706ff32dfb" }
              );
 
             context.Rooms.AddOrUpdate(
